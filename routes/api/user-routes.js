bd1b3b5e939eb28a2e13d21330f4235cb9fd98ef
ajcudd <ajcuddeback@gmail.com>
2021-01-23 10:31:52 -0500

    added Post methods in Post.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -71,7 +71,7 @@ router.post('/login', (req, res) => {
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in!' })
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
     })
 })
 
