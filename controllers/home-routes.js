afef489de633f5eb6a8ce6f58db156bb334c08dc
ajcudd <ajcuddeback@gmail.com>
2021-01-29 10:21:35 -0500

    added views template

---
+++ controllers/home-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('homepage', {
+        id: 1,
+        post_url: 'https://handlebarsjs.com/guide/',
+        title: 'Handlebars Docs',
+        creates_at: new Date(),
+        vote_count: 10,
+        comments: [{}, {}],
+        user: {
+            username: 'test_user'
+        }
+    });
+});
+
+module.exports = router;
--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const { post } = require('../routes');
 
 class Comment extends Model { }
 
