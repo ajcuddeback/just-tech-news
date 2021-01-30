6119120eea9dff6981feb9bc78d8e5209d17a515
ajcudd <ajcuddeback@gmail.com>
2021-01-30 10:16:39 -0500

    hide logout and login button based on user session

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,10 @@ router.get('/', (req, res) => {
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
             // pass a single post object into the homepage template
-            res.render('homepage', { posts });
+            res.render('homepage', { 
+                posts,
+                loggedIn: req.session.loggedIn
+             });
         })
         .catch(err => {
             console.log(err);
@@ -83,7 +86,10 @@ router.get('/post/:id', (req, res) => {
             const post = dbPostData.get({ plain: true });
 
             // pass data to the template
-            res.render('single-post', { post })
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            })
         })
         .catch(err => {
             console.log(err);
